b08c3fdbb1a86264f25f9338985b288d3d44ec28
Mykyta Kliuchka <74094674+hahafury@users.noreply.github.com>
2023-08-02 16:57:45 +0300

    Update checkly.config.ts

--- checkly.config.ts
+++ checkly.config.ts
@@ -1,8 +1,22 @@
-/**
- * @TODO Add checkly config
- */
-// import { defineConfig } from 'checkly'
-//
-// const config = defineConfig({})
-//
-// export default config
+import { defineConfig } from 'checkly'
+
+const config = defineConfig({
+  projectName: 'externals',
+  logicalId: 'externals',
+  repoUrl: 'https://github.com/hahafury/TETSREPO',
+  checks: {
+    frequency: 10,
+    locations: ['us-east-1', 'eu-west-1'],
+    tags: ['mac'],
+    checkMatch: '**/__checks__/**/*.check.ts',
+    browserChecks: {
+      testMatch: '**/__checks__/**/*.spec.ts',
+    },
+  },
+  cli: {
+    runLocation: 'eu-west-1',
+    reporters: ['list'],
+  },
+})
+
+export default config
